a323165e4b9b18f46d22623dfa65ee4c17d89471
ice2732 <ice2732@gmail.com>
2020-01-22 14:21:15 +0900

    api sample

---
+++ coding_style/js/api.js
@@ -0,0 +1,31 @@
+const debug = require('debug')('api');
+const express = require('express');
+const db = require('../storage');
+const router = express();
+
+// Set Nickname
+router.post('/account/nickname', async (req, res, next) => {
+  debug(req);
+
+  try {
+    if(!req.nickname) {
+      return next(`Not exist Nickname`);
+    }
+
+    const account = await db.account.get_account(req.account_idx);
+    if(!account) {
+      return next(`Not exist account. req.account_idx: ${req.account_idx}`);
+    }
+
+    account.nickname = req.nickname;
+    await account.save();
+  } catch(err) {
+    debug(err);
+
+    return next(`Error : ${err.name}`);
+  }
+
+  next();
+});
+
+module.exports = router;
